5ef6b65536d514ec8d8dbdfe8e544182d6d20f52
yulongtan <swifties@uw.edu>
2016-06-24 00:45:26 -0700

    initial testing code

--- src/Test.java
+++ src/Test.java
@@ -4,4 +4,16 @@
 
 public class Test {
 
+    public static void main(String[] stuff) {
+        ArrayStack<Integer> stack = new ArrayStack<>();
+        stack.push(2);
+        stack.push(7);
+        stack.push(5);
+        System.out.println("Stack: " + stack.toString()); // b[2, 7, 5]t
+        System.out.println("Size: " + stack.size()); // 3
+        int removal = stack.pop();  // 5
+        System.out.println("Just removed: " + removal);
+        System.out.println("Stack after removal: " + stack.toString()); //b[2, 7]
+        System.out.println("Size: " + stack.size()); // 2
+    }
 }
